9c324a7ca0e76134e105f8fd4a4ec37f5a3ee307
shukawam <shukawam@gmail.com>
2024-06-25 19:10:10 +0900

    Added initialize function for cohere

--- app/modules/cohere.py
+++ app/modules/cohere.py
@@ -5,7 +5,8 @@ from langfuse.decorators import observe
 class Cohere:
     def __init__(self, cohere_api_key: str, **kwargs):
         self.cohere_api_key = cohere_api_key
-        self.cohere_chat_model = ChatCohere(cohere_api_key=cohere_api_key)
+        self.cohere_chat_model = self._initialize_chat_cohere(cohere_api_key=cohere_api_key, kwargs=kwargs)
+        self.cohere_embeddings_model = self._initialize_embed_cohere(cohere_api_key=cohere_api_key, model_name="embed-multilingual-v3.0")
         self.callback_handler = kwargs.get("callback_handler")
 
     def chat(self, input: str, streaming: bool, max_tokens: int, temperature: float, k: int, p: float, frequency_penalty: float, presence_penalty: float):
@@ -34,3 +35,20 @@ class Cohere:
                 presence_penalty=presence_penalty
             )
             return response
+
+    def _initialize_chat_cohere(self, cohere_api_key: str, **kwargs) -> ChatCohere:
+        return ChatCohere(
+            cohere_api_key=cohere_api_key,
+            max_tokens=kwargs.get("max_tokens"),
+            temperature=kwargs.get("temperature"),
+            k=kwargs.get("k"),
+            p=kwargs.get("p"),
+            frequency_penalty=kwargs.get("frequency_penalty"),
+            presence_penalty=kwargs.get("presence_penalty")
+        )
+
+    def _initialize_embed_cohere(self, cohere_api_key, model_name: str) -> CohereEmbeddings:
+        return CohereEmbeddings(
+            cohere_api_key=cohere_api_key,
+            model=model_name
+        )
