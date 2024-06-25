c97fc998ec1cdc822c531d546e4443b06f153320
shukawam <shukawam@gmail.com>
2024-06-25 19:10:36 +0900

    Implemented simple rag using milvus

--- app/modules/cohere.py
+++ app/modules/cohere.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from langchain_cohere.chat_models import ChatCohere
-from langfuse.decorators import observe
+from langchain_cohere.embeddings import CohereEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain_milvus import Milvus
 
 class Cohere:
     def __init__(self, cohere_api_key: str, **kwargs):
@@ -8,18 +12,28 @@ class Cohere:
         self.cohere_chat_model = self._initialize_chat_cohere(cohere_api_key=cohere_api_key, kwargs=kwargs)
         self.cohere_embeddings_model = self._initialize_embed_cohere(cohere_api_key=cohere_api_key, model_name="embed-multilingual-v3.0")
         self.callback_handler = kwargs.get("callback_handler")
+        if ("milvus_uri" in kwargs) & ("collection_name" in kwargs):
+            self.milvus = Milvus(
+                embedding_function=self.cohere_embeddings_model,
+                collection_name=kwargs.get("collection_name"),
+                connection_args={"uri": kwargs.get("milvus_uri")}
+            )
+            self.retriever = self.milvus.as_retriever(
+                search_type=kwargs.get("search_type"),
+                search_kwargs={
+                    "k": kwargs.get("return_k"),
+                    "score_threshold": kwargs.get("score_threshold"),
+                    "fetch_k": kwargs.get("fetch_k"),
+                    "lambda_mult": kwargs.get("lambda_mult")
+                }
+            )
+        
 
-    def chat(self, input: str, streaming: bool, max_tokens: int, temperature: float, k: int, p: float, frequency_penalty: float, presence_penalty: float):
+    def chat(self, input: str, streaming: bool):
         if streaming == True:
             response = self.cohere_chat_model.stream(
                 input,
                 config={"callbacks": [self.callback_handler], "configurable": {"session_id": st.session_state["session_id"]}},
-                max_tokens=max_tokens,
-                temperature=temperature,
-                k=k,
-                p=p,
-                frequency_penalty=frequency_penalty,
-                presence_penalty=presence_penalty
             )
             for chunk in response:
                 yield chunk.content
@@ -27,12 +41,32 @@ class Cohere:
             response = self.cohere_chat_model.invoke(
                 input,
                 config={"callbacks": [self.callback_handler], "configurable": {"session_id": st.session_state["session_id"]}},
-                max_tokens=max_tokens,
-                temperature=temperature,
-                k=k,
-                p=p,
-                frequency_penalty=frequency_penalty,
-                presence_penalty=presence_penalty
+            )
+            return response
+
+    def chat_with_rag(self, input: str, streaming: bool):
+        langfuse = st.session_state["langfuse"]
+        prompt_template = PromptTemplate.from_template(
+            langfuse.get_prompt(name="demo-user-prompt", type="text").prompt,
+            template_format="jinja2"
+        )
+        chain = (
+            {"context": self.retriever, "question": RunnablePassthrough()}
+            | prompt_template
+            | self.cohere_chat_model
+            | StrOutputParser()
+        )
+        if streaming == True:
+            response = chain.stream(
+                input,
+                config={"callbacks": [self.callback_handler], "configurable": {"session_id": st.session_state["session_id"]}},
+            )
+            for chunk in response:
+                yield chunk
+        else:
+            response = chain.invoke(
+                input,
+                config={"callbacks": [self.callback_handler], "configurable": {"session_id": st.session_state["session_id"]}},
             )
             return response
 
